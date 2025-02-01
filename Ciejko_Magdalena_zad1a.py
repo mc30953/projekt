9f963ec1e2d73b09b5c4eb72042dbfefafe49aa3
mc30953 <mc30953@zpsb.pl>
2025-02-01 21:23:50 +0100

    zmiana

---
+++ Ciejko_Magdalena_zad1a.py
@@ -0,0 +1,7 @@
+#_-ZADANIE-_
+#używając end i sep przekształć funkcje print
+print("Kurs", "Programowania", "w")
+print("Pythonie")
+
+print("Kurs", "Programowania", "w", sep=" o-o-o-o-o ", end=" oxxx}{=======> ")
+print("Pythonie")
